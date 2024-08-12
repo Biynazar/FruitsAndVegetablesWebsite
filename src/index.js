d4601d7bfd5d29172c9a3ac3388e8cfce60c701a
Biynazar <bina78@list.ru>
2024-08-13 00:15:18 +0600

    add media css

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ let swiper = new Swiper(".home", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  let menu = document.getElementById('menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  
+  menu.addEventListener('click', () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  })
+  
+  window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
